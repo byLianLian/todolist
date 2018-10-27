6c5806ebbab0e4038ffdd9b4858e94b62f1d343c
byLianLian <bylianlianby@163.com>
2018-10-27 16:05:58 +0800

    Updates

---
+++ precache-manifest.dcaafff76f1524b2bec3c2486db8b4c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "6eceaf3512019062292b",
+    "url": "/todolist/static/js/main.6eceaf35.chunk.js"
+  },
+  {
+    "revision": "33572fb4a4748bddf753",
+    "url": "/todolist/static/js/1.33572fb4.chunk.js"
+  },
+  {
+    "revision": "6eceaf3512019062292b",
+    "url": "/todolist/static/css/main.d4dd7f78.chunk.css"
+  },
+  {
+    "revision": "67295e092ce513dac40fec3cff131954",
+    "url": "/todolist/index.html"
+  }
+];
